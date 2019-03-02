17e6068e6ce080e4ad52913f550f0cb66bdfd9af
Dusan Panic <dpanic@gmail.com>
2019-03-02 21:34:28 +0100

    init of build

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker stop gitscanner_instance
+docker rm gitscanner_instance 
+docker rmi gitscanner
+
+docker build -t gitscanner .
+docker run --name gitscanner_instance --restart=always -d -t gitscanner
+docker logs -f gitscanner_instance
